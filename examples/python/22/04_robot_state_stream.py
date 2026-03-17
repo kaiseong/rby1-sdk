4af6f6be74d246a5a6aa8d977753424dcf5a1551
sangmin.jeon <sangmin.jeon@rainbow-robotics.com>
2026-03-17 18:43:57 +0900

    feat(examples/python): add numbered example copies under 22

--- examples/python/22/04_robot_state_stream.py
+++ examples/python/22/04_robot_state_stream.py
@@ -1,8 +1,8 @@
-# Power Control Demo
-# This example demonstrates how to power on devices. See --help for arguments.
+# Robot State Stream Demo
+# This example is part of the RB-Y1 SDK examples. See --help for arguments.
 #
-# Usage example: 
-#     python 02_power.py --address 192.168.30.1:50051 --model a --device '.*'
+# Usage example:
+#     python 04_robot_state_stream.py --help
 #
 # Copyright (c) 2025 Rainbow Robotics. All rights reserved.
 #
@@ -12,36 +12,55 @@
 # the use or misuse of this demo code. Please use with caution and at your own discretion.
 
 import rby1_sdk as rby
+import time
 import argparse
+import numpy as np
 
 
-def main(address, model, power_device):
+def callback(robot_state, control_manager_state: rby.ControlManagerState):
+    np.set_printoptions(precision=3, suppress=True, floatmode="fixed")
+
+    print(robot_state)
+    print(control_manager_state)
+
+
+# or
+# def callback(robot_state):
+#     print(robot_state)
+
+
+def main(address, model, power):
     robot = rby.create_robot(address, model)
     if not robot.connect():
         print("Robot is not connected")
         exit(1)
-    # Power on the device if it's off.
-    if not robot.is_power_on(power_device):
-        rv = robot.power_on(power_device)
+    if not robot.is_power_on(power):
+        rv = robot.power_on(power)
         if not rv:
             print("Failed to power on")
             exit(1)
-    # Power off
-    # robot.power_off(power_device)
+
+    robot.start_state_update(callback, rate=10)  # Hz
+    try:
+        time.sleep(100)
+    except KeyboardInterrupt:
+        print("Stopping state update...")
+    finally:
+        robot.stop_state_update()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="02_power")
+    parser = argparse.ArgumentParser(description="04_robot_state_stream")
     parser.add_argument("--address", type=str, required=True, help="Robot address")
     parser.add_argument(
         "--model", type=str, default="a", help="Robot Model Name (default: 'a')"
     )
     parser.add_argument(
-        "--device",
+        "--power",
         type=str,
         default=".*",
         help="Power device name regex pattern (default: '.*')",
     )
     args = parser.parse_args()
 
-    main(address=args.address, model=args.model, power_device=args.device)
+    main(address=args.address, model=args.model, power=args.power)
